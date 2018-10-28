a8756ee193ca6678ffc4374e22567e51676d4f9c
Jason Park <parkjs814@gmail.com>
2018-10-28 18:40:53 -0400

    Parse more quickly

--- parse.js
+++ parse.js
@@ -1,39 +1,31 @@
-const cheerio = require('cheerio');
-
 const parse = html => {
-  const $ = cheerio.load(html);
-  const headerCols = $('.pagebodydiv > .datadisplaytable > tbody > tr > .ddtitle');
-
   const courses = {};
-  headerCols.each(sectionIndex => {
-    const headerCol = headerCols.eq(sectionIndex);
-    const bodyCol = headerCols.parent().next().children('.dddefault');
 
-    const headerText = headerCol.text();
-    const headerTokens = headerText.split(' - ');
-    const sectionId = headerTokens.pop();
-    const courseId = headerTokens.pop();
-    const crn = headerTokens.pop();
-    const courseTitle = headerTokens.join(' - ');
+  const startIndex = html.indexOf('<caption class="captiontext">Sections Found</caption>');
+  const endIndex = html.lastIndexOf('<table  CLASS="datadisplaytable" summary="This is for formatting of the bottom links." WIDTH="50%">');
+  const body = html.slice(startIndex, endIndex);
+  const sections = body.split('<tr>\n<th CLASS="ddtitle" scope="colgroup" >').slice(1);
 
-    const bodyText = bodyCol.text();
-    const scheduleType = /^(.+)\* Schedule Type$/m.exec(bodyText)[1];
-    const credits = Number.parseInt(/^\s*(\d\.\d{3}) Credits$/m.exec(bodyText)[1]);
+  sections.forEach(section => {
+    const [titlePart, descriptionPart, , ...meetingParts] = section.split('<tr>\n');
 
-    const meetingRows = bodyCol.find('.datadisplaytable > tbody > tr').slice(1);
-    const meetings = meetingRows.map(meetingIndex => {
-      const meetingRow = meetingRows.eq(meetingIndex);
-      const meetingText = meetingRow.text();
-      const meetingTokens = meetingText.split('\n');
-      let [, type, period, days, where, dateRange, scheduleType, instructors] = meetingTokens;
-      instructors = instructors.replace(/ +/g, ' ').split(', ');
+    const [, courseTitle, crn, courseId, sectionId] = /^<a href=".+">(.+) - (\d{5}) - (\w+ \d{4}) - (.+)<\/a>/.exec(titlePart);
+
+    const scheduleType = /^(.+)\* Schedule Type$/m.exec(descriptionPart)[1];
+    const credits = Number.parseInt(/^\s*(\d\.\d{3}) Credits$/m.exec(descriptionPart)[1]);
+
+    const meetings = meetingParts.map(meetingPart => {
+      let [type, period, days, where, dateRange, scheduleType, instructors] = meetingPart.split('\n').slice(0, 7)
+        .map(row => /^<td CLASS="dddefault">(.+)<\/td>$/.exec(row)[1]);
+      instructors = instructors.replace(/<\/?[^>]+(>|$)/g, '').replace(/ +/g, ' ').split(', ');
+
       return {
         period,
         days,
         where,
         instructors,
       };
-    }).get();
+    });
 
     if (!(courseId in courses)) {
       courses[courseId] = {
