538bb8ee250272bc6f8043b58adc352439b52792
Jason Park <parkjs814@gmail.com>
2019-03-11 00:26:39 -0400

    Add term_ins.json

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const download = require('./download');
 const parse = require('./parse');
 const upload = require('./upload');
 
-console.info('Listing...');
+console.info('Listing ...');
 list().then(terms_in => {
   return Promise.mapSeries(terms_in.slice(0, 2), term_in => {
     console.info(`Downloading ${term_in} ...`);
     return download(term_in)
       .then(html => {
-        console.info('Parsing...');
+        console.info('Parsing ...');
         return parse(html);
       })
       .then(courses => {
-        console.info('Uploading...');
+        console.info('Uploading ...');
         return upload(term_in, courses);
       });
   });
--- upload.js
+++ upload.js
@@ -12,30 +12,43 @@ const owner = 'parkjs814';
 const repo = 'gt-schedule-crawler';
 const branch = 'gh-pages';
 
+const uploadFile = (path, json, files) => {
+  const file = files.find(file => file.path === path);
+  const content = Buffer.from(JSON.stringify(json)).toString('base64');
+  return file ?
+    octokit.repos.updateFile({
+      owner,
+      repo,
+      path,
+      message: `Update ${path}`,
+      content,
+      sha: file.sha,
+      branch,
+    }).then(() => false) :
+    octokit.repos.createFile({
+      owner,
+      repo,
+      path,
+      message: `Create ${path}`,
+      content,
+      branch,
+    }).then(() => true);
+};
+
 const upload = (term_in, courses) =>
   octokit.repos.getContent({ owner, repo, path: '', ref: branch })
     .then(response => {
+      const files = response.data;
       const path = `${term_in}.json`;
-      const content = Buffer.from(JSON.stringify(courses)).toString('base64');
-      const file = response.data.find(file => file.path === path);
-      return file ?
-        octokit.repos.updateFile({
-          owner,
-          repo,
-          path,
-          message: `Update ${path}`,
-          content,
-          sha: file.sha,
-          branch,
-        }) :
-        octokit.repos.createFile({
-          owner,
-          repo,
-          path,
-          message: `Create ${path}`,
-          content,
-          branch,
-        });
+      return uploadFile(path, courses, files).then(created => {
+        if (created) {
+          const term_ins = files.map(file => file.path.split('.')[0]).filter(filename => /\d{6}/.test(filename));
+          term_ins.push(term_in);
+          term_ins.sort();
+          console.log(`Updating 'term_ins.json' ...`);
+          return uploadFile('term_ins.json', term_ins, files);
+        }
+      });
     });
 
 module.exports = upload;
