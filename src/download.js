43a7204f6f2b5c4cfa6560a27323d6d391331cab
Jinseo Park <jinseo@google.com>
2020-07-20 09:22:47 -0400

    Add attributes and grade basis

--- src/download.js
+++ src/download.js
@@ -1,8 +1,48 @@
 const axios = require('axios');
 
-const download = term => axios.post(
-  'https://oscar.gatech.edu/pls/bprod/bwckschd.p_get_crse_unsec',
-  `term_in=${term}&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_ptrm=%25&sel_instr=%25&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a`,
-).then(res => res.data);
+const concatParams = params => Object.entries(params).map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&');
+
+const download = (term, options = {}) => {
+  const {
+    subject = '',
+    course = '',
+    title = '',
+  } = options;
+  const dummyParams = [
+    'sel_subj',
+    'sel_day',
+    'sel_schd',
+    'sel_insm',
+    'sel_camp',
+    'sel_levl',
+    'sel_sess',
+    'sel_instr',
+    'sel_ptrm',
+    'sel_attr',
+  ].reduce((acc, dummyKey) => ({ ...acc, [dummyKey]: 'dummy' }), {});
+  const params = {
+    term_in: term,
+    sel_subj: subject,
+    sel_crse: course,
+    sel_title: title,
+    sel_schd: '%',
+    sel_from_cred: '',
+    sel_to_cred: '',
+    sel_camp: '%',
+    sel_ptrm: '%',
+    sel_instr: '%',
+    sel_attr: '%',
+    begin_hh: '0',
+    begin_mi: '0',
+    begin_ap: 'a',
+    end_hh: '0',
+    end_mi: '0',
+    end_ap: 'a',
+  };
+  return axios.post(
+    'https://oscar.gatech.edu/pls/bprod/bwckschd.p_get_crse_unsec',
+    [dummyParams, params].map(concatParams).join('&'),
+  ).then(res => res.data);
+};
 
 module.exports = download;
--- src/parse.js
+++ src/parse.js
@@ -12,12 +12,25 @@ const cache = (array, value) => {
   return index;
 };
 
+const map = (text, regexp, callback) => {
+  const array = [];
+  let results;
+  while (results = regexp.exec(text)) {
+    array.push(callback(results));
+  }
+  return array;
+};
+
 const parse = html => {
   const courses = {};
-  const dateRanges = [];
-  const scheduleTypes = [];
-  const campuses = [];
-  const instructionalMethods = [];
+  const caches = {
+    periods: [],
+    dateRanges: [],
+    scheduleTypes: [],
+    campuses: [],
+    attributes: [],
+    gradeBases: [],
+  };
   const updatedAt = new Date();
 
   const startIndex = html.indexOf('<caption class="captiontext">Sections Found</caption>');
@@ -30,23 +43,34 @@ const parse = html => {
 
     const [, courseTitle, crn, courseId, sectionId] = /^<a href=".+">(.+) - (\d{5}) - (\w+ \w+) - (.+)<\/a>/.exec(titlePart);
 
+    const fields = map(descriptionPart, /^<SPAN class="fieldlabeltext">(.+): <\/SPAN>(.+)$/mg, results => {
+      const [, key, value] = results;
+      return { key, value };
+    }).reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
+    const attributesKey = 'Attributes';
+    const attributes = attributesKey in fields ?
+      fields[attributesKey].split(',').map(attribute => attribute.trim()) :
+      [];
+    const gradeBasis = fields['Grade Basis'];
+
     const credits = Number.parseInt(match(descriptionPart, /(\d+\.\d{3}) Credits$/m));
     const scheduleType = match(descriptionPart, /^(.+) Schedule Type$/m);
     const campus = match(descriptionPart, /^(.+) Campus$/m);
-    const instructionalMethod = match(descriptionPart, /^(.+) Instructional Method$/m);
 
-    const scheduleTypeIndex = cache(scheduleTypes, scheduleType);
-    const campusIndex = cache(campuses, campus);
-    const instructionalMethodIndex = cache(instructionalMethods, instructionalMethod);
+    const scheduleTypeIndex = cache(caches.scheduleTypes, scheduleType);
+    const campusIndex = cache(caches.campuses, campus);
+    const attributeIndices = attributes.map(attribute => cache(caches.attributes, attribute));
+    const gradeBasisIndex = cache(caches.gradeBases, gradeBasis);
 
     const meetings = meetingParts.map(meetingPart => {
       let [type, period, days, where, dateRange, scheduleType, instructors] = meetingPart.split('\n').slice(0, 7)
         .map(row => row.replace(/<\/?[^>]+(>|$)/g, ''));
       instructors = instructors.replace(/ +/g, ' ').split(', ');
-      const dateRangeIndex = cache(dateRanges, dateRange);
+      const periodIndex = cache(caches.periods, period);
+      const dateRangeIndex = cache(caches.dateRanges, dateRange);
 
       return [
-        period,
+        periodIndex,
         days,
         where,
         instructors,
@@ -68,11 +92,12 @@ const parse = html => {
       credits,
       scheduleTypeIndex,
       campusIndex,
-      instructionalMethodIndex,
+      attributeIndices,
+      gradeBasisIndex,
     ];
   });
 
-  return { courses, dateRanges, scheduleTypes, campuses, instructionalMethods, updatedAt };
+  return { courses, caches, updatedAt };
 };
 
 module.exports = parse;
