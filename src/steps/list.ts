382b9f9ae0b0e314689db5a5bf8464c7ddab408c
Nathan Gong <nathangong1234@gmail.com>
2023-03-15 01:24:45 -0700

    Obtain terms from new endpoint (#4)
    * This PR updates the endpoint for fetching the list of all available terms

--- src/steps/list.ts
+++ src/steps/list.ts
@@ -1,16 +1,23 @@
 import axios from "axios";
-import { extract } from "../utils";
+import { getIntConfig } from "../utils";
+
+const PADDING = 4;
+const NUM_TERMS = getIntConfig("NUM_TERMS") ?? 2;
+
+export type TermData = {
+  code: string;
+  description: string;
+};
 
 export async function list(): Promise<string[]> {
-  const response = await axios.post<string>(
-    "https://oscar.gatech.edu/pls/bprod/bwckctlg.p_disp_dyn_ctlg"
+  const queryNum = NUM_TERMS + PADDING;
+
+  const response = await axios.post<TermData[]>(
+    `https://registration.banner.gatech.edu/StudentRegistrationSsb/ssb/classSearch/getTerms?searchTerm=&offset=1&max=${queryNum}`
   );
   const { data } = response;
-  const terms = extract(
-    data,
-    /<OPTION VALUE="(\d+)">/g,
-    (execArray) => execArray[1]
-  );
+
+  const terms = data.map((term) => term.code);
   return terms.filter((term) => {
     const month = Number(term.slice(4));
     return month >= 1 && month <= 12;
